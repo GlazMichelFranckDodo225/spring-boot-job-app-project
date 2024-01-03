69bb1e1a58eb50f7a983c5f14b19ff7895a9763c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-03 10:32:37 +0100

    Project Initialization

---
+++ src/main/java/com/dgmf/springbootjobappproject/SpringBootJobAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.springbootjobappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJobAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJobAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/springbootjobappproject/SpringBootJobAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.springbootjobappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
